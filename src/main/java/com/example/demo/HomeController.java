5123e29268346e6201847c713817275c85b0a871
Terry Farria <tfjteachus@gmail.com>
2018-03-27 09:43:22 -0400

    recent test

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
